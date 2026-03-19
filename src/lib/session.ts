5b140291ad80b5aa89316f298136cc8fea7cd102
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-03-19 14:16:53 +0900

    session verification

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,14 +1,82 @@
+import "server-only";
 import { cookies } from "next/headers";
+import { SignJWT, jwtVerify } from "jose";
+import { redirect } from "next/navigation";
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+type SessionPayload = {
+  userId: string;
+};
+
+async function encrypt(payload: SessionPayload) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("1h")
+    .sign(encodedKey);
+}
+
+async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload as SessionPayload;
+  } catch (error) {
+    console.log("Failed to verify session");
+    return null;
+  }
+}
 
 export async function createSession(userId: string) {
-  const expiresAt = new Date(Date.now() + 1000 * 60 * 60)
-  const cookieStore = await cookies()
+  const expiresAt = new Date(Date.now() + 1000 * 60 * 60);
+  const session = await encrypt({ userId });
+  const cookieStore = await cookies();
 
-  cookieStore.set("session", userId, {
+  cookieStore.set("session", session, {
     httpOnly: true,
     secure: true,
     expires: expiresAt,
     sameSite: "lax",
     path: "/",
-  })
+  });
+}
+
+export async function updateSession() {
+  const session = (await cookies()).get("session")?.value;
+  const payload = await decrypt(session);
+
+  if (!session || !payload) {
+    return null;
+  }
+
+  const newSession = await encrypt({ userId: payload.userId });
+  const expiresAt = new Date(Date.now() + 1000 * 60 * 60);
+
+  const cookieStore = await cookies();
+  cookieStore.set("session", newSession, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+}
+
+export async function verifySession() {
+  const session = (await cookies()).get("session")?.value;
+  const payload = await decrypt(session);
+
+  if (!payload?.userId) {
+    redirect("/login");
+  }
+
+  return { userId: payload.userId };
 }
