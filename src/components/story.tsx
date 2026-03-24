bd8b0260d0eb1ac4ce8a5649d5b4052f79ee9a55
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-03-24 15:20:49 +0900

    stylize story

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -14,19 +14,33 @@ type Props = {
 
 export default function Story({ story }: Props) {
   return (
-    <div>
-      <div>
-	<Link href={story.url}>{story.title}</Link>
+    <div className="group flex items-center gap-3 py-2 px-1 rounded hover:bg-stone-100">
+      <div className="flex flex-col items-center shrink-0 w-8">
+        {story.score}
       </div>
-      <div>
-	{story.score} {story.score === 1 ? "upvote" : "upvotes"}
-      </div>
-      submitted by {story.author.username}
-      <div>
-	<Link href={`/story/${story.id}`}>
-	  {story._count.comments}{" "}
-	  {story._count.comments === 1 ? "comment" : "comments"}
-	</Link>
+
+      <div className="flex flex-col">
+        <div className="flex flex-row items-center gap-1">
+          <a href={story.url} target="_blank" rel="noopener noreferrer">
+            {story.title}
+          </a>
+          {story.url && (
+            <span className="text-xs text-stone-400 font-mono truncate]">
+              ({new URL(story.url).hostname.replace("www.", "")})
+            </span>
+          )}
+        </div>
+
+        <div className="flex flex-row gap-1 text-xs text-stone-500 font-mono">
+          <span>submitted by {story.author.username}</span>
+          <span className="text-stone-400 mx-0.5">|</span>
+          <div>
+            <Link href={`/story/${story.id}`}>
+              {story._count.comments}{" "}
+              {story._count.comments === 1 ? "comment" : "comments"}
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
