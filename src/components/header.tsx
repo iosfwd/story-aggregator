24bac7bc128fb40f4b189decb180cb19ac59a59d
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-03-24 13:22:03 +0900

    logout button styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { verifySession } from "@/lib/session";
-import logoutButton from "@/components/logout-button";
+import LogoutButton from "@/components/logout-button";
 
 export default async function Header() {
   const session = await verifySession().catch(() => null);
@@ -8,15 +8,15 @@ export default async function Header() {
   return (
     <header className="bg-pink-500 px-4 py-1">
       <nav className="max-w-4xl mx-auto flex items-center gap-2 text-sm">
-	<Link href="/">Home</Link>
-	<span className="text-black/40"> | </span>
-	<Link href="/submit">Submit</Link>
-	{!session && <span className="text-black/40"> | </span>}
-	{!session && <Link href={"/login"}>Login</Link>}
-	{!session && <span className="text-black/40"> | </span>}
-	{!session && <Link href={"/signup"}>Sign up</Link>}
-	<span className="text-black/40"> | </span>
-	{session && <button onClick={logoutButton}>Logout</button>}
+        <Link href="/">Home</Link>
+        <span className="text-black/40"> | </span>
+        <Link href="/submit">Submit</Link>
+        <div className="ml-auto">
+          {!session && <Link href={"/login"}>Login</Link>}
+          {!session && <span className="text-black/40"> | </span>}
+          {!session && <Link href={"/signup"}>Sign up</Link>}
+          {session && <LogoutButton />}
+        </div>
       </nav>
     </header>
   );
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -5,8 +5,8 @@ import { logoutUser } from "@/app/login/actions";
 
 export default function LogoutButton() {
   return (
-    <Form action={logoutUser}>
-      <button type="submit">Logout</button>
+    <Form action={logoutUser} className="inline-block">
+      <button type="submit" className="bg-none border-none cursor-pointer">Logout</button>
     </Form>
   );
 }
