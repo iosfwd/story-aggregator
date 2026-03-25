771ab4767c18c7376ece5062e4e96bf4e2e7f070
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-03-25 14:04:40 +0900

    formatting

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -6,7 +6,9 @@ import { logoutUser } from "@/app/login/actions";
 export default function LogoutButton() {
   return (
     <Form action={logoutUser} className="inline-block">
-      <button type="submit" className="bg-none border-none cursor-pointer">Logout</button>
+      <button type="submit" className="bg-none border-none cursor-pointer">
+        Logout
+      </button>
     </Form>
   );
 }
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -5,12 +5,12 @@ export default function SignupForm() {
   return (
     <Form action={createUser}>
       <div>
-	<label htmlFor="username">Username</label>
-	<input id="username" name="username" placeholder="Username" required />
+        <label htmlFor="username">Username</label>
+        <input id="username" name="username" placeholder="Username" required />
       </div>
       <div>
-	<label htmlFor="password">Password</label>
-	<input id="password" name="password" placeholder="Password" required />
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" placeholder="Password" required />
       </div>
       <button type="submit">Sign up</button>
     </Form>
