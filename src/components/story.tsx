92a0c9f7b1a4854e117013a5bd3350f5b9b3abe8
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-03-26 15:29:09 +0900

    add vote buttons to story component

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -1,6 +1,7 @@
 import { Prisma } from "@/app/generated/prisma/client";
 import Link from "next/link";
 import { timeAgo } from "@/lib/utils";
+import VoteButtons from "@/components/vote-buttons";
 
 type StoryWithMeta = Prisma.StoryGetPayload<{
   include: {
@@ -16,34 +17,32 @@ type Props = {
 export default function Story({ story }: Props) {
   return (
     <div className="group flex items-center gap-3 rounded px-1 py-2 hover:bg-stone-100">
-      <div className="flex w-8 shrink-0 flex-col items-center">
-        {story.score}
-      </div>
+      <VoteButtons storyId={story.id} score={story.score} />
 
       <div className="flex flex-col">
-        <div className="flex flex-row items-center gap-1">
-          <a href={story.url} target="_blank" rel="noopener noreferrer">
-            {story.title}
-          </a>
-          {story.url && (
-            <span className="truncate font-mono text-xs text-stone-400">
-              ({new URL(story.url).hostname.replace("www.", "")})
-            </span>
-          )}
-        </div>
+	<div className="flex flex-row items-center gap-1">
+	  <a href={story.url} target="_blank" rel="noopener noreferrer">
+	    {story.title}
+	  </a>
+	  {story.url && (
+	    <span className="truncate font-mono text-xs text-stone-400">
+	      ({new URL(story.url).hostname.replace("www.", "")})
+	    </span>
+	  )}
+	</div>
 
-        <div className="flex flex-row gap-1 font-mono text-xs text-stone-500">
-          <span>
-            submitted by {story.author.username} {timeAgo(story.createdAt)} ago
-          </span>
-          <span className="mx-0.5 text-stone-400">|</span>
-          <div>
-            <Link href={`/story/${story.id}`}>
-              {story._count.comments}{" "}
-              {story._count.comments === 1 ? "comment" : "comments"}
-            </Link>
-          </div>
-        </div>
+	<div className="flex flex-row gap-1 font-mono text-xs text-stone-500">
+	  <span>
+	    submitted by {story.author.username} {timeAgo(story.createdAt)} ago
+	  </span>
+	  <span className="mx-0.5 text-stone-400">|</span>
+	  <div>
+	    <Link href={`/story/${story.id}`}>
+	      {story._count.comments}{" "}
+	      {story._count.comments === 1 ? "comment" : "comments"}
+	    </Link>
+	  </div>
+	</div>
       </div>
     </div>
   );
