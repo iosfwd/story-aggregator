84d19f4012d73ac35124c14acedb62dc91979ad0
Jaakko Matias Vuohtoniemi <jaakko.vuohtoniemi@helsinki.fi>
2026-04-08 18:59:34 +0900

    redirect if logged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,14 @@
 import LoginForm from "@/components/login-form";
+import { verifySession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const session = await verifySession().catch(() => null);
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div>
       <LoginForm />
